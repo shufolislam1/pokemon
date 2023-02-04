beecb90728cb2930a44c6ed5469f85858b1d3575
shufol islam <shufolislam1255@gmail.com>
2023-02-05 01:34:07 +0600

    trying to understand graphql

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Hero = () => {
+    const [data, setData] = useState({});
+    const gqlQuery = `query pokemons($limit: Int, $name: String!) {
+        pokemons(limit: $limit, name: $name) {
+          count
+          next
+          previous
+          status
+          message
+          id
+          name
+          results {
+            url
+            name
+            image
+            types
+          }
+          sprites {
+            front_default
+          }
+          moves {
+            move {
+              name
+            }
+          }
+          types {
+            type {
+              name
+            }
+          }
+        }
+      }`;
+    //   const details = `query pokemon($name: String!) {
+    //     pokemon(name: $name) {
+    //       id
+    //       name
+    //       sprites {
+    //         front_default
+    //       }
+    //       moves {
+    //         move {
+    //           name
+    //         }
+    //       }
+    //       types {
+    //         type {
+    //           name
+    //         }
+    //       }
+    //     }
+    //   }`
+      const gqlVariables = {
+        limit: 10,
+        "name": "ditto"
+      };
+      
+      fetch('https://graphql-pokeapi.graphcdn.app/', {
+        credentials: 'omit',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query: gqlQuery,
+          variables: gqlVariables,
+        }),
+        method: 'POST',
+      })
+        .then((res) => res.json())
+        // .then((res) => setData(res.data.pokemons.results[0]));
+        .then((res) => console.log(res));
+
     return (
         <div style={{ backgroundImage: `url(https://i.ibb.co/fQBhJSb/Background.png)`, height: 1000, width: 1800 }}>
             <div className='flex justify-center'>
@@ -9,10 +77,10 @@ const Hero = () => {
             <div className='relative overflow-hidden bg-no-repeat bg-cover '>
                 <div className="card w-96 bg-base-100 hover:bg-blue-400 shadow-xl max-w-xs hover:scale-110 transition duration-300 ease-in-out">
                     <figure className="px-10 pt-10">
-                        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                        <img src={data?.image} alt="Shoes" className="rounded-xl" />
                     </figure>
                     <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
+                        <h2 className="card-title">{data?.name}</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions">
                             <button className="btn btn-primary">Buy Now</button>
