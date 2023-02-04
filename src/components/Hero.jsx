669e01aa47902b283f3c668d3ac1d6079bea5b68
shufol islam <shufolislam1255@gmail.com>
2023-02-04 18:22:05 +0600

    design of banner done without data

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,24 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div style={{backgroundImage: `url(https://i.ibb.co/fQBhJSb/Background.png)`, height:1000, width:1800}}>
+        <div style={{ backgroundImage: `url(https://i.ibb.co/fQBhJSb/Background.png)`, height: 1000, width: 1800 }}>
             <div className='flex justify-center'>
-                <img style={{marginTop: 50 , marginLeft:150}} src="https://i.ibb.co/sqX7Qc2/Logo.png" alt="pokemon" srcSet="" />
+                <img style={{ marginTop: 50, marginLeft: 150 }} src="https://i.ibb.co/sqX7Qc2/Logo.png" alt="pokemon" srcSet="" />
             </div>
-            <div></div>
+            <div className='relative overflow-hidden bg-no-repeat bg-cover '>
+                <div className="card w-96 bg-base-100 shadow-xl max-w-xs hover:scale-110 transition duration-300 ease-in-out">
+                    <figure className="px-10 pt-10">
+                        <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
