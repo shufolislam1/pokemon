cee5fc588820bfbf80fbda3badc5d9de9bc67f90
Nihal <shufolislam1255@gmail.com>
2023-02-05 16:31:33 +0600

    image and name loading from api

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import InfoCards from "./InfoCards";
 
 const Hero = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   const gqlQuery = `query pokemons($limit: Int) {
         pokemons(limit: $limit) {
           results {
@@ -9,74 +10,52 @@ const Hero = () => {
             image
           } 
         }
-      }`;
-  //   const details = `query pokemon($name: String!) {
-  //     pokemon(name: $name) {
-  //       id
-  //       name
-  //       sprites {
-  //         front_default
-  //       }
-  //       moves {
-  //         move {
-  //           name
-  //         }
-  //       }
-  //       types {
-  //         type {
-  //           name
-  //         }
-  //       }
-  //     }
-  //   }`
+      }
+
+
+      `;
   const gqlVariables = {
     limit: 10,
+    // id: "1",
     // "name": "ditto"
   };
 
-  fetch('https://graphql-pokeapi.graphcdn.app/', {
-    credentials: 'omit',
-    headers: { 'Content-Type': 'application/json' },
+  fetch("https://graphql-pokeapi.graphcdn.app/", {
+    credentials: "omit",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       query: gqlQuery,
       variables: gqlVariables,
     }),
-    method: 'POST',
+    method: "POST",
   })
     .then((res) => res.json())
-    .then((res) => setData(res.data.pokemons.results));
-  // .then((res) => console.log(res.data.pokemons.results));
+    .then((res) => setData(res?.data?.pokemons?.results));
+  // .then((res) => console.log(res));
 
   return (
-    <div style={{ backgroundImage: `url(https://i.ibb.co/fQBhJSb/Background.png)`, height: 1000, width: 1800 }}>
-      <div className='flex justify-center'>
-        <img style={{ marginTop: 50, marginLeft: 150 }} src="https://i.ibb.co/sqX7Qc2/Logo.png" alt="pokemon" srcSet="" />
+    <div
+      style={{
+        backgroundImage: `url(https://i.ibb.co/fQBhJSb/Background.png)`,
+        height: 1000,
+        width: 1800,
+      }}
+    >
+      <div className="flex justify-center">
+        <img
+          style={{ marginTop: 50, marginLeft: 150 }}
+          src="https://i.ibb.co/sqX7Qc2/Logo.png"
+          alt="pokemon"
+          srcSet=""
+        />
       </div>
-      {
-        data?.map((oneData, index) => {
-          // return (
-          //   <div key={index}>
-          //     <div className='relative overflow-hidden bg-no-repeat bg-cover '>
-          //       <div className="card w-96 bg-base-100 hover:bg-blue-400 shadow-xl max-w-xs hover:scale-110 transition duration-300 ease-in-out">
-          //           <figure className="px-10 pt-10">
-          //               <img src={oneData?.image} alt="Shoes" className="rounded-xl" />
-          //           </figure>
-          //           <div className="card-body items-center text-center">
-          //               <h2 className="card-title">{oneData?.name}</h2>
-          //               <p>If a dog chews shoes whose shoes does he choose?</p>
-          //               <div className="card-actions">
-          //                   <button className="btn btn-primary">Buy Now</button>
-          //               </div>
-          //           </div>
-          //       </div>
-          //   </div>
-          //   </div>
-          // )
-          console.log(oneData)
-        })
-      }
+      <div className="grid grid-cols-5 gap-10  mt-10">
+        {data?.map((oneData, index) => (
+          <InfoCards key={index} oneData={oneData}></InfoCards>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Hero;
--- src/components/InfoCards.jsx
+++ src/components/InfoCards.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const InfoCards = () => {
-    return (
-        <div>
-            
+const InfoCards = ({ oneData }) => {
+//   console.log(oneData);
+  return (
+    <div>
+      <div className="relative overflow-hidden bg-no-repeat bg-cover ">
+        <div className="card w-96 bg-base-100 hover:bg-blue-400 shadow-xl max-w-xs hover:scale-110 transition duration-300 ease-in-out">
+          <figure className="px-10 pt-10">
+            <img src={oneData?.image} alt="Shoes" className="rounded-xl" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">{oneData?.name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default InfoCards;
